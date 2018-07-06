80fbab5c53f6a967e7f4a8c6fbc174289b3c4862
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-06 19:36:39 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,9 +8,16 @@ class Post < ActiveRecord::Base
   private
 
   def click_bait
+<<<<<<< HEAD
     if self.title != nil
       unless
         (self.title.include?("Won't Believe")) || (self.title.include?("Secret")) || (self.title.include?("Top")) || (self.title.include?("Guess"))
+=======
+    if self.valid?
+      if
+        !(self.title.include?("Won't Believe")) || !(self.title.include?("Secret")) || !(self.title.include?("Top")) || !(self.title.include?("Guess"))
+      #["Won't Believe", "Secret", "Top", "Guess"].any? {|b| (record.title).include? b}
+>>>>>>> 22610f3e4cfc817cd75c2341b99968f8049b85b2
         self.errors[:title] << "Edit the title to make it click-baity!"
       end
     end
